d8609fb6186f19b373db6fa107bca6911da97994
Ivan Tung <ivantung@me.com>
2022-03-30 15:43:08 +0800

    Fix more formatting issues

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_staticImage: true
+  unstable_staticImage: true,
 })
 module.exports = withNextra(
   {
     i18n: {
       locales: ['en', 'zh', 'ja'],
-      defaultLocale: 'en'
-    }
+      defaultLocale: 'en',
+    },
   },
   {
     poweredByHeader: false,
-    reactStrictMode: true
+    reactStrictMode: true,
   }
 )
